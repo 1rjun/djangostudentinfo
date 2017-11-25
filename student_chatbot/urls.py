3e9196f3be07c39f7036d2bf352b53573d599f50
1rjun <arjunsumbria@gmail.com>
2017-11-25 14:32:37 +0530

    edit the login to login1

--- student_chatbot/urls.py
+++ student_chatbot/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^register$',views.register,name='register'),
-    url(r'^login$',views.login,name="login"),
+    url(r'^login1$',views.login,name="login"),
     url(r'^register_done',views.login,name="register_done")
 ]
