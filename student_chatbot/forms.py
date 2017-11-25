ba573fcc7d828f68de4dd200039e6fe8a910b446
1rjun <arjunsumbria@gmail.com>
2017-11-25 14:32:21 +0530

    just working on forms

--- student_chatbot/forms.py
+++ student_chatbot/forms.py
@@ -1,54 +1,51 @@
 from django import forms
 from .models import Class , Student
+from django.contrib.auth import (
+    authenticate,
+    get_user_model
+)
+
+User = get_user_model()
+
 #A login form 
 class LoginForm(forms.Form):
     
-    your_id = forms.IntegerField(label="Your id ")
+    your_id = forms.CharField(label="Your id ")
     your_pass = forms.CharField(label="Your pass",
     widget=forms.PasswordInput,min_length=8)
 
-    def clean_your_id(self):
+    def clean(self):
         your_id = self.cleaned_data["your_id"]
-        return your_id
+        your_pass = self.cleaned_data["your_pass"]
+        user = authenticate(username=your_id,password=your_pass)
+        
+        if not user:
+            raise forms.ValidationError("User doesnt exist")
+        
+        if not user.check_password(your_pass):
+            raise forms.ValidationError("Incorrect Password")
+
+        
+        return super(LoginForm,self).clean()
+
+
+
 
 
 #A registration Form
 class StudentForm(forms.ModelForm):
-
-    Rollno = forms.IntegerField(label="Rollno")
-    Password = forms.CharField(label="Password",
-    widget=forms.PasswordInput,min_length=8)
-    Re_Password=forms.CharField(label="Confirm-Password",
-    widget=forms.PasswordInput,min_length=8)
-    
-    
-
 # Clean method which checks whether password matches or not
 #and returns the cleaned data and put it in the register form 
-## view
-    def clean(self):
-
-        #it will call the RegisterForm and clean its values
-        #and save in a variable
-        cleaned_data = super(StudentForm, self).clean()
-        #make the instance of Password 
-        Pass = cleaned_data["Password"]
-        Re_Pass = cleaned_data["Re_Password"]
-        if Pass != Re_Pass:
-            raise forms.ValidationError("Password doesn't Match")
-        return cleaned_data
-    
+## view    
     #Model form
     class Meta:
         model = Student
-        Fields = ["Rollno","Password"]
-
+        fields = ["Rollno","Password","Semester"]
+        #exclude = ("Semester",)
+     
 
 class Class(forms.ModelForm):
-    Dob = forms.DateTimeField(label="Dob")
-    Name = forms.CharField(label="Name")
-    Semester = forms.CharField(label="Semester")
-
+    
     class Meta:
         model = Class
         fields =["Dob","Name","Sem"]
