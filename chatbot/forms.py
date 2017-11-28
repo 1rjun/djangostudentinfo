0fd84db1e702dbb373be7767ed344bc5d6a20b2a
1rjun <arjunsumbria@gmail.com>
2017-11-28 20:45:45 +0530

    added the login form in my login view and its connectivity

--- chatbot/forms.py
+++ chatbot/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import ButtonHolder,Submit,Layout
@@ -13,5 +13,17 @@ class RegistrationForm(UserCreationForm):
             'password2',
             ButtonHolder(
                 Submit('register', 'Register', css_class = "btn-primary")
+            )
+        )
+
+class LoginForm(AuthenticationForm):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'username',
+            'password',
+            ButtonHolder(
+                Submit('login','Login', css_class = "btn-success")
             )
         )
